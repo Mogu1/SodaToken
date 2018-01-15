b1a525e5b3674ae563beec3f90797dfa1aff2762
Inga Bemman (R0otChiXor) <R0otChiXor@gmail.com>
2018-01-15 12:00:09 -0700

    updated amount of total supply

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -228,8 +228,8 @@ UniValue disconnectnode(const UniValue& params, bool fHelp)
             "\nArguments:\n"
             "1. \"node\"     (string, required) The node (see getpeerinfo for nodes)\n"
             "\nExamples:\n"
-            + HelpExampleCli("disconnectnode", "\"192.168.0.6:8666\"")
-            + HelpExampleRpc("disconnectnode", "\"192.168.0.6:8666\"")
+            + HelpExampleCli("disconnectnode", "\"192.168.0.6:8777\"")
+            + HelpExampleRpc("disconnectnode", "\"192.168.0.6:8777\"")
         );
 
     CNode* pNode = FindNode(params[0].get_str());
