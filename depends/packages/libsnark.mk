69d77c9658196cfe7a322ff1735db84a786ec1c1
alex <alex.s.contact@gmail.com>
2018-01-18 17:39:14 +0100

    libsnark dependence fix

--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -1,9 +1,9 @@
 package=libsnark
-$(package)_download_path=https://github.com/Soda TokencashOfficial/$(package)/releases/download/v20170131/
+$(package)_download_path=https://github.com/BinaryOmen/$(package)/releases/download/v20180117/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 
-$(package)_sha256_hash=0c243deec42b133948cd5d77848f7988a79e82e5bf570c39d9a96ee7a42f5302
-$(package)_git_commit=3854b20c25e8bc567aab2b558dec84d45f4a3e73
+$(package)_sha256_hash=66540a5b8ad712b5c47b008a957eaad03d77ef30bb4d15d6b9590dabe1948b50
+$(package)_git_commit=9e6b19ff15bc19fba5da1707ba18e7f160e5ed07
 
 define $(package)_set_vars
     $(package)_build_env=CC="$($(package)_cc)" CXX="$($(package)_cxx)"
@@ -12,8 +12,12 @@ endef
 
 $(package)_dependencies=libgmp libsodium
 
+define $(package)_preprocess_cmds
+  cmake .
+endef
+
 define $(package)_build_cmds
-  CXXFLAGS="-fPIC -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2"
+  CXXFLAGS="-fPIC -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2"
 endef
 
 define $(package)_stage_cmds
